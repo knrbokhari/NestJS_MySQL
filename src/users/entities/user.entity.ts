7e8c835de7449daa81c14ed9604774b383533135
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-03-07 19:52:20 +0600

    User.entity.ts added

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from 'src/typeorm/entities/User';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty({ required: false, nullable: true })
+  authStrategy: string | null;
+}
